247c6668c8aeb57d806b1418b447d8ce3dada71a
Wesley Newcomb <wesleyjnewcomb@gmail.com>
2017-07-31 15:05:31 -0400

    added create action

--- app/controllers/api/v1/decks_controller.rb
+++ app/controllers/api/v1/decks_controller.rb
@@ -3,4 +3,19 @@ class Api::V1::DecksController < ApplicationController
     @decks = Deck.all
     render json: @decks, adapter: :json
   end
+
+  def create
+    data = JSON.parse(request.body.read)
+    @new_deck = Deck.new({
+      user: current_user,
+      name: data['deck']['name'],
+      description: data['deck']['description']
+    })
+
+    if @new_deck.save
+      render json: @new_deck, adapter: :json
+    else
+      render json: { errors: @new_deck.errors.full_messages }, status: 422
+    end
+  end
 end
--- spec/controllers/api/v1/decks_controller_spec.rb
+++ spec/controllers/api/v1/decks_controller_spec.rb
@@ -40,4 +40,64 @@ RSpec.describe Api::V1::DecksController, type: :controller do
       end
     end
   end
+
+  describe 'POST#create' do
+    before(:each) do
+      @user = FactoryGirl.create(:user)
+      session[:user_id] = @user.id
+    end
+
+    after(:each) { session[:user_id] = nil }
+
+
+    context 'correct data' do
+      let(:deck_data) do
+        {
+          deck: {
+            name: 'deck name',
+            description: 'deck description'
+          }
+        }
+      end
+      it 'creates a new Deck' do
+        expect do
+          post :create, body: deck_data.to_json
+        end.to change { Deck.count }.by 1
+      end
+
+      it 'renders a status of 201' do
+        post :create, body: deck_data.to_json
+        expect(response.status).to eq 200
+      end
+
+      it 'renders a JSON representing the new Deck' do
+        post :create, body: deck_data.to_json
+        returned_json = JSON.parse(response.body)
+        expect(returned_json['deck']['name']).to eq deck_data[:deck][:name]
+        expect(returned_json['deck']['description']).to eq deck_data[:deck][:description]
+        user_data = returned_json['deck']['user']
+        expect(user_data['id']).to eq @user.id
+      end
+    end
+
+    context 'incorrect data' do
+      let(:deck_data) do
+        {
+          deck: {
+            description: 'deck description'
+          }
+        }
+      end
+      it 'does not create a new Deck' do
+        expect do
+          post :create, body: deck_data.to_json
+        end.to change { Deck.count }.by 0
+      end
+
+      it 'renders a status of 422' do
+        post :create, body: deck_data.to_json
+        expect(response.status).to eq 422
+      end
+    end
+  end
 end
