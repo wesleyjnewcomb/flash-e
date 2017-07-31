1a38f0d87f317cec971dc6c3f2360dd32f606b41
Wesley Newcomb <wesleyjnewcomb@gmail.com>
2017-07-31 15:53:20 -0400

    added side1 and side2 validations

--- spec/models/card_spec.rb
+++ spec/models/card_spec.rb
@@ -2,5 +2,7 @@ require 'rails_helper'
 
 RSpec.describe Card, type: :model do
   it { should belong_to :deck }
+  it { should validate_presence_of :side1 }
+  it { should validate_presence_of :side2 }
   it { should validate_presence_of :deck }
 end
