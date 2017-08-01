065204d261a516b5422c667c8ed2da536cf61e8b
Wesley Newcomb <wesleyjnewcomb@gmail.com>
2017-08-01 10:58:41 -0400

    create_cards and update_crads helper methods

--- app/models/deck.rb
+++ app/models/deck.rb
@@ -2,4 +2,33 @@ class Deck < ApplicationRecord
   belongs_to :user
   has_many :cards
   validates :name, presence: true
+
+  def create_cards(card_data_array)
+    cards = card_data_array.map do |card_data|
+      Card.new(card_data.merge(deck: self))
+    end
+
+    cards.each do |card|
+      return nil unless card.save
+    end
+    cards
+  end
+
+  def update_cards(card_data_array)
+    updated_cards = []
+    card_data_array.each do |card_data|
+      card = Card.find(card_data['id'])
+      card.side1 = card_data['side1']
+      card.side2 = card_data['side2']
+      if card.valid?
+        if card.changed?
+          card.save
+          updated_cards << card
+        end
+      else
+        return nil
+      end
+    end
+    updated_cards
+  end
 end
