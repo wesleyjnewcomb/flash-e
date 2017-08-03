6feb688d108b0643c1687b430a5e3da3cfd4ded8
Wesley Newcomb <wesleyjnewcomb@gmail.com>
2017-08-03 13:35:28 -0400

    deck tile tests

---
+++ app/javascript/react/test/components/DeckTileSpec.js
@@ -0,0 +1,42 @@
+import React from 'react'
+import { mount } from 'enzyme'
+import jasmineEnzyme from 'jasmine-enzyme'
+import createRouterContext from 'react-router-test-context'
+
+import DeckTile from '../../src/components/DeckTile'
+
+describe('TextField', () => {
+  let deck, wrapper
+  let name = 'Deck Name'
+  let description = 'Deck description'
+  let deckData = {
+    id: 1,
+    name: name,
+    description: description,
+    cards: [
+      { id: 1, side1: 'card 1 side 1', side2: 'card 1 side 2'},
+      { id: 2, side2: 'card 2 side 1', side2: 'card 2 side 2'}
+    ]
+  }
+  DeckTile.contextTypes = {
+    router: React.PropTypes.object
+  }
+  beforeEach(() => {
+    const context = createRouterContext()
+    wrapper = mount(<DeckTile deck={deckData} />, { context })
+  })
+
+  it('should render the DeckTile component', () => {
+    expect(wrapper.find(DeckTile)).toBePresent()
+  })
+  it('should renders the name as an h2', () => {
+    expect(wrapper.find('h2')).toIncludeText(name)
+  })
+  it('should not render the cards at first', () => {
+    expect(wrapper.find('table')).not.toBePresent()
+  })
+  it('should render the cards if showingCards is true in state', () => {
+    wrapper.setState({ showingCards: true })
+    expect(wrapper.find('table')).toBePresent()
+  })
+})
