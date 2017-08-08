faee613c13a4701fdc7910fab3237bdcbb0fe853
Wesley Newcomb <wesleyjnewcomb@gmail.com>
2017-08-08 11:36:49 -0400

    added User show page

---
+++ app/javascript/react/src/containers/UserShowContainer.js
@@ -0,0 +1,53 @@
+import React, { Component } from 'react'
+
+import DeckIndexContainer from './DeckIndexContainer'
+
+class UserShowContainer extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      user: {},
+      fetched: false
+    }
+  }
+
+  componentDidMount() {
+    fetch(`/api/v1/users/${this.props.match.params.id}`)
+    .then(response => {
+      if (response.ok) {
+        return response.json()
+      }
+    })
+    .then(response => {
+      this.setState({ user: response.user, fetched: true })
+    })
+  }
+
+  render() {
+    let user = this.state.user
+    let deckIndexFetchPath = `/api/v1/users/${user.id}/decks`
+    return (
+      <div className='user-show-container'>
+        <h1 className='text-center'>User Profile</h1>
+        <div className='row'>
+          <div className='small-12 large-6 columns'>
+            <div className='panel'>
+              <img src={user.image} />
+              <h4 className='text-center'>{user.name}</h4>
+              <p className='text-right'><i>({user.email})</i></p>
+            </div>
+          </div>
+          <div className='small-12 large-6 columns'>
+            <DeckIndexContainer
+              title={`${this.state.user.name}'s Decks`}
+              fetchPatch={deckIndexFetchPath}
+              showForm={false}
+            />
+          </div>
+        </div>
+      </div>
+    )
+  }
+}
+
+export default UserShowContainer
