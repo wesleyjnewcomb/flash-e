288a3265abc27608335b72b7fcec6958c29dba43
Wesley Newcomb <wesleyjnewcomb@gmail.com>
2017-08-08 11:21:30 -0400

    added show action

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,6 +1,14 @@
 class Api::V1::UsersController < ApplicationController
   skip_before_action :verify_authenticity_token
 
+  def show
+    if User.exists?(params[:id])
+      render json: User.find(params[:id]), adapter: :json
+    else
+      render json: { errors: ['User not found'] }, status: 404
+    end
+  end
+
   def current
     if current_user
       render json: current_user, adapter: :json
