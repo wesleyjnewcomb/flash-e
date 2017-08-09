bf036ce62ed2efb1f2711f65cbed45c64afa75b1
Wesley Newcomb <wesleyjnewcomb@gmail.com>
2017-08-09 14:30:00 -0400

    updated avatar size

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -4,7 +4,7 @@ FactoryGirl.define do
     uid "117346877858607749947"
     name { Faker::Name.name }
     email { Faker::Internet.email }
-    image { Faker::LoremPixel.image("48x48") }
+    image { Faker::LoremPixel.image("640x640") }
     oauth_token { Faker::Internet.password(130) }
     oauth_expires_at "2017-07-29 13:37:43"
   end
